3776cb12b862252d0a0a8e79909bb8a8d1e24691
SharjeelBaig0508 <51065492+SharjeelBaig0508@users.noreply.github.com>
2019-05-29 17:38:54 +0500

    Assignment Update

--- Marksheet Program.py
+++ Marksheet Program.py
@@ -15,14 +15,14 @@ print("+" + "-" * 63 +"+")
 total_marks = 550
 
 subject_total_marks = { 
-	"English" : 75, 
-	"Urdu" : 75, 
-	"Maths" : 75, 
-	"Physics" : 75, 
-	"Chemistry" : 75, 
-	"Computer" : 75, 
+	"English Compulsory" : 75, 
+	"Urdu Compulsory" : 75, 
+	"Mathematics Comp." : 75, 
+	"Physics Inter XI" : 75, 
+	"Chemistry Inter XI" : 75, 
+	"Computer Science" : 75, 
 	"Pakistan Studies" : 50, 
-	"Islamiat" : 50 
+	"Islamiat Studies" : 50 
 }
 
 subject_obtained_marks = []
@@ -40,23 +40,6 @@ for subject, marks in subject_total_marks.items():
 
 obtained_marks = sum(subject_obtained_marks)
 
-print("+" + "-" * 63 + "+")
-print("Student Name :", student_name)
-print("Father Name :", student_father)
-print("+" + "-" * 15 + "+" + "-" * 15 + "+" + "-" * 15 + "+")
-print("|Subject\t|Obtained Marks\t|Total Marks\t|")
-print("+" + "-" * 15 + "+" + "-" * 15 + "+" + "-" * 15 + "+")
-index = 0
-for subject, marks in subject_total_marks.items():
-	print("|" + subject[0 : 6] + "\t", subject_obtained_marks[index], marks, sep = "\t|\t", end = "\t|\n")
-	index += 1
-print("+" + "-" * 15 + "+" + "-" * 15 + "+" + "-" * 15 + "+")
-print("Total Obtained Marks :", obtained_marks)
-percentage = (obtained_marks / total_marks) * 100
-if percentage >= 40:
-	print("Student Passed with", percentage,"%")
-else:
-	print("Student Failed with", percentage, "%")
 def grader(perc):
 	if perc <= 100 and perc >= 90:
 		return "A+"
@@ -72,6 +55,25 @@ def grader(perc):
 		return "E"
 	else:
 		return "F"
+
+print("+" + "-" * 63 + "+")
+print("Student Name :", student_name)
+print("Father Name :", student_father)
+print("+" + "-" * 23 + "+" + "-" * 15 + "+" + "-" * 15 + "+" + "-" * 15 + "+")
+print("|Subject\t\t|Obtained Marks\t|Total Marks\t|Grade\t\t|")
+print("+" + "-" * 23 + "+" + "-" * 15 + "+" + "-" * 15 + "+" + "-" * 15 + "+")
+index = 0
+for subject, marks in subject_total_marks.items():
+	percent = (subject_obtained_marks[index] / marks) * 100
+	print("|" + subject, subject_obtained_marks[index], marks, grader(percent), sep = "\t|\t", end = "\t|\n")
+	index += 1
+print("+" + "-" * 23 + "+" + "-" * 15 + "+" + "-" * 15 + "+" + "-" * 15 + "+")
+print("Total Obtained Marks :", obtained_marks)
+percentage = (obtained_marks / total_marks) * 100
+if percentage >= 40:
+	print("Student Passed with", percentage,"%")
+else:
+	print("Student Failed with", percentage, "%")
 
 print("Grade :", grader(percentage))
 print("+" + "-" * 63 + "+")
